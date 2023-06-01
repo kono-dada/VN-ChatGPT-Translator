7e372b5e91a8f262d6a1166d942805bb3715135d
kono-dada <121090029@link.cuhk.edu.cn>
2023-06-01 20:14:12 +0800

    modified

--- configs.py
+++ configs.py
@@ -6,20 +6,15 @@ text_dir = 'text'
 
 all_text = os.listdir(text_dir)
 
-total_accounts = 4
+total_accounts = 1  # 总共账号数量
 
-# put the session urls here
-# 请在这里放置session的url。如果你不知道这是什么，请参考README.md
+# 请在这里放置session的url。
 urls = {
-    1: 'https://chat.openai.com/c/cba96978-4255-4713-82cd-f2cced52a4da',
-    2: 'https://chat.openai.com/c/425d1705-7e79-46cf-b1e7-29f6ce59fe1f',
-    3: 'https://chat.openai.com/c/f893aac8-d7fb-4267-ba22-709a5efce5c1',
-    4: 'https://chat.openai.com/c/b6720ac2-400e-46b4-8c32-5cd2acf693dd'
+    1: 'https://chat.openai.com/c/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
 }
 
-# put the cookies here
-# 请在这里放置cookies。如果你不知道这是什么，请参考README.md
-cookies_mapping = {1: 'cookie1.json', 2: 'cookie2.json', 3: 'cookie3.json', 4: 'cookie4.json'}
+# 请在这里放置cookies。
+cookies_mapping = {1: 'cookie_example.json'}
 
 prompt = 'please translate the following galgame conversations from Japanese to Chinese. ' + \
         'Please preserve escape characters like \\r\\n or \\" and strictly keep the JSON format of the provided content. ' + \
