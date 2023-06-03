87efba9f681f190785c8afe3495f9d34e71a45a4
kono-dada <121090029@link.cuhk.edu.cn>
2023-06-03 12:09:31 +0800

    modified

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from translator import Translator
 import sys
-import time
+import os
 
 
 # get an argument from the console
 # the argument is the account number
 
 if __name__ == '__main__':
+    if not os.path.exists('translated'):
+        os.mkdir('translated')
     account = int(sys.argv[1])
     start_time = sys.argv[2]
     translator = Translator(account, start_time)  # 可以添加headless=False来显示浏览器，方便调试
